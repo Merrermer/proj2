7d298108da715d6bc139e2d71baae80cf24eb401
Merrermer <merrermer66@gmail.com>
2024-07-13 08:01:45 -0400

    Update dictionarize.py

--- dictionarize.py
+++ dictionarize.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+# convert the content of the orderbook to dictionaries of price and value
 def message_to_orderbookd(data):
     b = data.get('bids')
     a = data.get('asks')
@@ -10,7 +10,7 @@ def message_to_orderbookd(data):
     d['asks'] = dict(np.array(a, dtype=float))
     d['bids'] = dict(np.array(b, dtype=float))
     return d
-
+# update local orderbook with diff data
 def update_orderbook(orderbook, data):
     a = dict(np.array(data.get('a'), dtype=float))
     b = dict(np.array(data.get('b'), dtype=float))
@@ -19,9 +19,9 @@ def update_orderbook(orderbook, data):
     orderbook = clean(orderbook)
     return orderbook
 
+# delete the prices in orderbook that are traded or cancelled
 def clean(orderbook):
     d = orderbook.copy()
     d['asks'] = {k: v for k, v in orderbook['asks'].items() if v != 0}
     d['bids'] = {k: v for k, v in orderbook['bids'].items() if v != 0}
-    print('cleaned')
     return d
